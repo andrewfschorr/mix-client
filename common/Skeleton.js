d21b6b8ab9872415c13fe4b6dc965c52c3a0c704
Andrew Schorr <afs35mm@gmail.com>
2020-03-22 16:55:58 -0400

    lower cased header

--- common/Skeleton.js
+++ common/Skeleton.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Header from 'components/Header';
+import Header from 'components/header';
 // import Head from 'common/head';
 // import Footer from 'common/footer';
 
